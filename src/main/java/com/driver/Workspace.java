888e00c1d0d2efb2db6da198ebf659f5eddf8f64
Pragya Kumari <pragyakumari121@gmail.com>
2024-03-24 13:59:54 +0530

    finally

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -30,16 +30,10 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-
-        calendar.sort((m1, m2) -> m1.getEndTime().compareTo( m2.getEndTime()));
+        ArrayList<Meeting> curr = new ArrayList<>(calendar);
+        curr.sort((m1, m2) -> m1.getEndTime().compareTo( m2.getEndTime()));
         int count=0;
-//        int currEndTime=-1;
-//        for(int i=0;i<calendar.size();i++){
-//            if(currEndTime<calendar.get(i).getStartTime()){
-//                currEndTime= calendar.get(i).getEndTime();
-//                count++;
-//            }
-//        }
+//
         Meeting previousMeeting = null;
 
         for (Meeting meeting : calendar) {
@@ -49,7 +43,7 @@ public class Workspace extends Gmail{
                 previousMeeting = meeting;
             }
         }
-        return count;
+        return count+1;
 
     }
 }
